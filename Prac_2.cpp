476d3498f63a3a55d6d8a0b0c9da35b65c2fab6e
Chintanvck <88276991+Chintanvck@users.noreply.github.com>
2021-08-01 18:33:48 +0530

    Add files via upload

---
+++ Prac_2.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<fstream>
+
+
+using namespace std;
+
+int main()
+{
+    fstream f1,f2,f3;
+    string i1,i2;
+
+    f1.open("Source1.txt",ios::in);
+    f2.open("Source2.txt",ios::in);
+    f3.open("Destination.txt",ios::out);
+
+
+
+    while(getline(f1,i1))
+    {
+        f3<<i1;
+        f3<<endl;
+    }
+    while(getline(f2,i2))
+    {
+        f3<<i2;
+        f3<<endl;
+    }
+    f1.close();
+    f2.close();
+    f3.close();
+}
